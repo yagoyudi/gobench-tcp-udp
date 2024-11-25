602694f8e3db0f48f029e6ae35ad1065700dd143
yago yudi <yagofuruta@protonmail.com>
2024-11-25 16:17:24 -0300

    feat: 5gb and 10gb

--- internal/cmd/const.go
+++ internal/cmd/const.go
@@ -3,22 +3,26 @@ package cmd
 import "fmt"
 
 const (
-	tenMB         = 10 * 1024 * 1024
-	hundredMB     = 100 * 1024 * 1024
-	fiveHundredMB = 500 * 1024 * 1024
+	oneMB         = 1024 * 1024 * 1024
+	hundredMB     = 100 * oneMB
+	fiveHundredMB = 500 * oneMB
 	oneGB         = 1024 * 1024 * 1024
+	fiveGB        = 5 * oneGB
+	tenGB         = 10 * oneGB
 )
 
 func parseTotalFlag(total string) (int, error) {
 	switch total {
-	case "10mb":
-		return tenMB, nil
 	case "100mb":
 		return hundredMB, nil
 	case "500mb":
 		return fiveHundredMB, nil
 	case "1gb":
 		return oneGB, nil
+	case "5gb":
+		return fiveGB, nil
+	case "10gb":
+		return tenGB, nil
 	}
 	return 0, fmt.Errorf("invalid amount")
 }
