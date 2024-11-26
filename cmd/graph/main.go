4974f63aa27ab284fc591c9ee77823b4362de9c8
yago yudi <yagofuruta@protonmail.com>
2024-11-26 11:01:02 -0300

    feat: exemplo de gráfico

--- cmd/graph/main.go
+++ cmd/graph/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"math/rand"
-
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/plotutil"
@@ -10,38 +8,74 @@ import (
 )
 
 func main() {
-	rand.Seed(int64(0))
-
 	p := plot.New()
 
-	p.Title.Text = "Plotutil example"
-	p.X.Label.Text = "X"
-	p.Y.Label.Text = "Y"
+	p.Title.Text = "TCP vs UDP"
+	p.Y.Label.Text = "Quantidade de bytes (MB)"
+	p.X.Label.Text = "Tempo total de envio (s)"
 
 	err := plotutil.AddLinePoints(p,
-		"First", randomPoints(15),
-		"Second", randomPoints(15),
-		"Third", randomPoints(15))
+		"TCP", tcpPoints(),
+		"UDP", udpPoints(),
+	)
 	if err != nil {
 		panic(err)
 	}
 
 	// Save the plot to a PNG file.
-	if err := p.Save(4*vg.Inch, 4*vg.Inch, "points.png"); err != nil {
+	if err := p.Save(5*vg.Inch, 5*vg.Inch, "points.png"); err != nil {
 		panic(err)
 	}
 }
 
-// randomPoints returns some random x, y points.
-func randomPoints(n int) plotter.XYs {
-	pts := make(plotter.XYs, n)
+// Sobre esses dados:
+// - Em ambos os casos, todos os bytes chegaram.
+// - Feito por wifi.
+
+func udpPoints() plotter.XYs {
+	pts := make(plotter.XYs, 5)
+	yValues := []float64{
+		100,   // 100mb
+		500,   // 500mb
+		1000,  // 1gb
+		5000,  // 5gb
+		10000, // 10gb
+	}
+	xValues := []float64{
+		2.556249124,
+		12.51920333,
+		25.515900918,
+		129.323465052,
+		260.609461813,
+	}
+
 	for i := range pts {
-		if i == 0 {
-			pts[i].X = rand.Float64()
-		} else {
-			pts[i].X = pts[i-1].X + rand.Float64()
-		}
-		pts[i].Y = pts[i].X + 10*rand.Float64()
+		pts[i].X = xValues[i]
+		pts[i].Y = yValues[i]
+	}
+	return pts
+}
+
+func tcpPoints() plotter.XYs {
+	pts := make(plotter.XYs, 5)
+	yValues := []float64{
+		100,
+		500,
+		1000,
+		5000,
+		10000,
+	}
+	xValues := []float64{
+		1.478977309,
+		6.938055906,
+		14.18291412,
+		69.708409905,
+		141.318771262,
+	}
+
+	for i := range pts {
+		pts[i].X = xValues[i]
+		pts[i].Y = yValues[i]
 	}
 	return pts
 }
