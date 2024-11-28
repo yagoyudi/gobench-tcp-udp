4e9c27e124009620c580607ed8367eb90ff6450f
yago yudi <yagofuruta@protonmail.com>
2024-11-28 07:15:04 -0300

    feat: 250mb, 500mb, 1gb, 2gb e 4gb

--- internal/cmd/const.go
+++ internal/cmd/const.go
@@ -1,28 +1,27 @@
 package cmd
 
 const (
-	oneMB         = 1 * 1024 * 1024
-	tenMB         = 10 * oneMB
-	hundredMB     = 100 * oneMB
-	fiveHundredMB = 500 * oneMB
-	oneGB         = 1 * 1024 * 1024 * 1024
-	fiveGB        = 5 * oneGB
-	tenGB         = 10 * oneGB
+	oneMB                = 1 * 1024 * 1024
+	twoHundredAndFiftyMB = 250 * oneMB
+	fiveHundredMB        = 500 * oneMB
+	oneGB                = 1 * 1024 * 1024 * 1024
+	twoGB                = 2 * oneGB
+	fourGB               = 4 * oneGB
 )
 
 func parseTotalFlag(total string) int {
 	switch total {
-	case "100mb":
-		return hundredMB
+	case "250mb":
+		return twoHundredAndFiftyMB
 	case "500mb":
 		return fiveHundredMB
 	case "1gb":
 		return oneGB
-	case "5gb":
-		return fiveGB
-	case "10gb":
-		return tenGB
+	case "2gb":
+		return twoGB
+	case "4gb":
+		return fourGB
 	default:
-		return tenMB
+		return twoHundredAndFiftyMB
 	}
 }
